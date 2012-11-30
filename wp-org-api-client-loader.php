bef2c8a8ced0ab96c805f95563d3ee30dd913ea0
Mike Schinkel <mikeschinkel@gmail.com>
2012-11-30 04:24:33 -0500

    Renamed the constants to reflect published name of plugin.

--- wp-org-api-client-loader.php
+++ wp-org-api-client-loader.php
@@ -2,14 +2,14 @@
 /*
  * This code is run after Imperative validates that the required libraries are available and loaded.
  */
-define( 'WP_PLUGIN_INFO_DIR', dirname( __FILE__ ) );
-define( 'WP_PLUGIN_INFO_VER', '0.0.0' );
-define( 'WP_PLUGIN_INFO_MIN_PHP', '5.2.4' );
-define( 'WP_PLUGIN_INFO_MIN_WP', '3.2' );
+define( 'WP_ORG_API_CLIENT_DIR', dirname( __FILE__ ) );
+define( 'WP_ORG_API_CLIENT_VER', '0.0.0' );
+define( 'WP_ORG_API_CLIENT_MIN_PHP', '5.2.4' );
+define( 'WP_ORG_API_CLIENT_MIN_WP', '3.2' );
 
-require(WP_PLUGIN_INFO_DIR . '/classes/class-plugin-info-api-client.php');
-require(WP_PLUGIN_INFO_DIR . '/classes/class-plugin-list-slugs-api-client.php');
-//require( WP_PLUGIN_INFO_DIR . '/classes/class-transients.php');
-//require( WP_PLUGIN_INFO_DIR . '/classes/class-plugin.php');
+require( WP_ORG_API_CLIENT_DIR . '/classes/class-plugin-info-api-client.php' );
+require( WP_ORG_API_CLIENT_DIR . '/classes/class-plugin-list-slugs-api-client.php' );
+//require( WP_ORG_API_CLIENT_DIR . '/classes/class-transients.php');
+//require( WP_ORG_API_CLIENT_DIR . '/classes/class-plugin.php');
 
 
